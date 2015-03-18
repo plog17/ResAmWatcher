878cc4c5a046c7c170786b2272b84158cb433aea
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-18 17:36:13 -0400

    without crypt..heroku doesn't like stdin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var SecureConf = require('secure-conf');
 var sconf = new SecureConf();
 var secureConfigFile = "./config/default.json.enc";
 var watcher = require('./Watcher/reserveAmericaWatcher');
-
+var config = require('./config/default.json');
 var express = require('express');
 var app = express();
 var config;
@@ -16,16 +16,22 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-sconf.decryptFile(secureConfigFile, function(err, file, content) {
-  if (err) {
-    console.log('Unable to retrieve the configuration contents.');
-  } else {
-    console.log("Config loaded");
-    config = JSON.parse(content);
-    var port = config.Server.port;
-    app.listen(port, function() {
-      console.log("Node app is running at localhost:" + port);
-      watcher.watch(config);
-    });
-  }
+app.listen(config.Server.port, function() {
+  console.log("Node app is running at localhost:" + config.Server.port);
+  watcher.watch(config);
 });
+
+
+// sconf.decryptFile(secureConfigFile, function(err, file, content) {
+//   if (err) {
+//     console.log('Unable to retrieve the configuration contents.');
+//   } else {
+//     console.log("Config loaded");
+//     config = JSON.parse(content);
+//     var port = config.Server.port;
+//     app.listen(port, function() {
+//       console.log("Node app is running at localhost:" + port);
+//       watcher.watch(config);
+//     });
+//   }
+// });
