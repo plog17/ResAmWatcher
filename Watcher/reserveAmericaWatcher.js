5fc8ecf5342b318e1b8465180d31c8d75deac8c2
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-18 20:31:28 -0400

    got it

--- Watcher/reserveAmericaWatcher.js
+++ Watcher/reserveAmericaWatcher.js
@@ -16,7 +16,7 @@ exports.watch = function(config) {
 
   var millisecondsWait = config.Watcher.waitTimeInSeconds * 1000;
   var pass = config.Emailer.pass;
-  var waitUntilResendEmail = 1000 * 60 * 60 * 3;
+  var waitUntilResendEmail = 1000 * 60 * 60 * 1;
   var body1Anastasia = "siteType=-1&csite=&eqplen=&maxpeople=&hookup=&range=1&arvdate=Fri+May+01+2015&enddate=&lengthOfStay=1&contractCode=FL&siteTypeFilter=ALL&parkId=281003&submitSiteForm=true&search=site&currentMaximumWindow=12";
   var body2Anastasia = "siteType=-1&csite=&eqplen=&maxpeople=&hookup=&range=1&arvdate=Sat+May+02+2015&enddate=&lengthOfStay=1&contractCode=FL&siteTypeFilter=ALL&parkId=281003&submitSiteForm=true&search=site&currentMaximumWindow=12";
   var body1FortClinch = "contractCode=FL&parkId=281027&siteTypeFilter=ALL&availStatus=&submitSiteForm=true&search=site&campingDate=Fri+May+01+2015&lengthOfStay=1&campingDateFlex=&currentMaximumWindow=12&contractDefaultMaxWindow=MS%3A24%2CLT%3A18%2CGA%3A24%2CSC%3A13&stateDefaultMaxWindow=MS%3A24%2CGA%3A24%2CSC%3A13&defaultMaximumWindow=12&loop=&siteCode=&lookingFor=&camping_2001_3013=&camping_2001_218=&camping_2002_3013=&camping_2002_218=&camping_2003_3012=&camping_3100_3012=&camping_10001_3012=&camping_10001_218=&camping_3101_3012=&camping_3101_218=&camping_9002_3012=&camping_9002_3013=&camping_9002_218=&camping_9001_3012=&camping_9001_218=&camping_3001_3013=&camping_2004_3013=&camping_2004_3012=&camping_3102_3012=";
@@ -27,7 +27,7 @@ exports.watch = function(config) {
     var emailSendA2 = false;
     var emailSendFC1 = false;
     var emailSendFC2 = false;
-  }, 60 * 1000);
+  }, waitUntilResendEmail);
 
   setInterval(function() {
     console.log("I am doing my check");
